0722be451c66b6b5ca1fb4907166e603b620a5ac
pkuzmicki <kpatryk042@gmail.com>
2025-05-28 00:51:08 +0200

    walking between rooms fixed

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,10 @@
 
 
 sf::Vector2f lerp(sf::Vector2f start, sf::Vector2f end, float t) {
+    //sf::Vector2f distance(end - start);
+    //std::cout<<distance.x<<distance.y<<std::endl;
 
-    sf::Vector2f distance(end - start);
-    std::cout<<distance.x<<distance.y<<std::endl;
-
-    if ((end - start).x < 0.1 && (end - start).y < 0.01) {
+    if ((end - start).x < 0.1 && (end - start).x > -0.1 && (end - start).y < 0.1  && (end - start).y > -0.1){
         return end;
     }
 
@@ -44,38 +43,34 @@ int main() {
     window.setFramerateLimit(60);
 
     bool isInBattleMode = false;
-
-    float x = 100.f;
-    float y = 100.f;
-
-    bool canMove = true;
-
     
-    sf::Vector2f playerPosition(200.f, 200.f);
+    sf::Vector2f playerPosition(roomWidth/2, roomHeight/2);
+    sf::Vector2f movePosition;
+    sf::Vector2f endPosition(playerPosition);
 
     sf::Vector2f worldOffset(0.f, 0.f);
 
     sf::FileInputStream i0("../res/WizardOfTheMoon-YG5y.ttf");
     sf::Font font(i0);
 
-    sf::FileInputStream inputS("../res/goblin.png");
-    sf::Texture player1(inputS);
-    sf::Sprite sprite1(player1);
-    sprite1.setScale(sf::Vector2f(3.f, 3.f));
-    sprite1.setPosition(playerPosition);
+    sf::FileInputStream iS0("../res/goblin.png");
+    sf::Texture pTxt(iS0);
+    sf::Sprite playerSprite(pTxt);
+    playerSprite.setScale(sf::Vector2f(3.f, 3.f));
+    playerSprite.setPosition(sf::Vector2f(playerPosition.x - 48, playerPosition.y - 30));
 
-    sf::FileInputStream in_t01("../res/missing.png");
-    sf::Texture tile01(in_t01);
+    sf::FileInputStream iT0("../res/missing.png");
+    sf::Texture tile01(iT0);
     sf::Sprite tile_missing(tile01);
     tile_missing.setScale(sf::Vector2f(3.f, 3.f));
 
-    sf::FileInputStream in_t02("../res/gravel.png");
-    sf::Texture tile02(in_t02);
+    sf::FileInputStream iT1("../res/gravel.png");
+    sf::Texture tile02(iT1);
     sf::Sprite tile_gravel(tile02);
     tile_gravel.setScale(sf::Vector2f(3.f, 3.f));
 
-    sf::FileInputStream in_t03("../res/gravel.png");
-    sf::Texture tile03(in_t03);
+    sf::FileInputStream iT2("../res/path.png");
+    sf::Texture tile03(iT2);
     sf::Sprite tile_path(tile03);
     tile_path.setScale(sf::Vector2f(3.f, 3.f));
 
@@ -105,6 +100,7 @@ int main() {
     std::uniform_int_distribution rand(1, 6);
     int dice_num;
 
+
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
@@ -112,22 +108,16 @@ int main() {
             } 
         }
 
-
-
-         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X) && clock.getElapsedTime() > sf::seconds(1.f)) {
-                isInBattleMode = !isInBattleMode;
-                x = 100.f;
-                y = 100.f;
-                dice_num = rand(gen);
-                clock.restart();
-            }
-
-            input.updateKeys();
-            input.updateMouse();
-        
-
         window.clear();
+        input.updateKeys();
+        input.updateMouse();
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X) && clock.getElapsedTime() > sf::seconds(1.f)) {
+            isInBattleMode = !isInBattleMode;
+            dice_num = rand(gen);
+            clock.restart();
+        }
+       
         if (!isInBattleMode) {
             // for (int row = 0; row < tilesInHeight; row++) {
             //     for (int col = 0; col < tilesInWidth; col++) {
@@ -139,11 +129,11 @@ int main() {
             for (int row = 0; row < tilesInHeight; row++) {
                 for (int col = 0; col < tilesInWidth; col++) {
                     //std::cout<<layout[col][row]<<"\n";
-                    if (layout[col][row] == 1) {
+                    if (layout[col][row] == 2) {
                         tile_path.setPosition(sf::Vector2f(tileSize * col + worldOffset.x, tileSize * row + worldOffset.y));
                         window.draw(tile_path);
                     }
-                    if (layout[col][row] == 2) {
+                    if (layout[col][row] == 1) {
                         tile_gravel.setPosition(sf::Vector2f(tileSize * col + worldOffset.x, tileSize * row + worldOffset.y));  
                         window.draw(tile_gravel);
                     }  
@@ -151,19 +141,17 @@ int main() {
             }
 
         
+            
+            movePosition = sf::Vector2f(input.getMousePosition().x, input.getMousePosition().y);
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                endPosition = movePosition;
+            }
 
-            sf::Vector2f startPosition(playerPosition);
-            sf::Vector2f endPosition(sf::Vector2f(input.getMousePosition().x, input.getMousePosition().y));
-
-           
             if (input.getMousePosition().x < roomWidth && input.getMousePosition().x > 0) {
-                if (input.getMousePosition().y < roomWidth && input.getMousePosition().y > 0) {
-
+                if (input.getMousePosition().y < roomHeight && input.getMousePosition().y > 0) {
                     if (input.getMousePosition().x != 0 && input.getMousePosition().y != 0) {
                         if (playerPosition != endPosition) {
-                            playerPosition = lerp(startPosition, endPosition, 0.1);
-                        } else {
-                            //std::cout<<"stoje sztywno\n";
+                            playerPosition = lerp(playerPosition, endPosition, 0.1);
                         } 
                     }
                 }
@@ -173,35 +161,38 @@ int main() {
             
             
             if (playerPosition.y < 32) {
-                //worldOffset.y += roomHeight;
+                worldOffset.y += roomHeight;
                 playerPosition.y += roomHeight - tileSize;
+                endPosition.y = roomHeight - tileSize/2;
             }
 
             if (playerPosition.y > window.getSize().y - 32) {
                 worldOffset.y -= roomHeight;
                 playerPosition.y -= roomHeight - tileSize;
+                endPosition.y = tileSize/2;
             } 
 
             if (playerPosition.x < 32) {
                 worldOffset.x += roomWidth;
                 playerPosition.x += roomWidth - tileSize;
+                endPosition.x = roomWidth - tileSize/2;
             }
 
             if (playerPosition.x > window.getSize().x - 32) {
                 worldOffset.x -= roomWidth;
                 playerPosition.x -= roomWidth - tileSize;
+                endPosition.x = tileSize/2;
             }
 
-            // if (printLocation.getElapsedTime() > sf::seconds(0.5)) {
-            //     std::cout<<"Gracz: "<<startPosition.x<<" "<<startPosition.y<<"\n";
-            //     printLocation.restart();
-            // }
-            sprite1.setPosition(playerPosition);
-            window.draw(sprite1);
+            if (printLocation.getElapsedTime() > sf::seconds(0.25) && playerPosition != endPosition) {
+                std::cout<<"Gracz: "<<playerPosition.x<<" "<<playerPosition.y<<"\n";
+                std::cout<<"Koniec: "<<endPosition.x<<" "<<endPosition.y<<"\n";
+                printLocation.restart();
+            }
 
-
-            //printf("%f, %f\n", playerPosition.x, playerPosition.y);
-            //printf("%f, %f\n", endPosition.x, endPosition.y);
+            
+            playerSprite.setPosition(sf::Vector2f(playerPosition.x - 48, playerPosition.y - 30));
+            window.draw(playerSprite);
         }
 
         if (isInBattleMode) {
@@ -216,15 +207,6 @@ int main() {
 
             window.draw(table);
             window.draw(dice);
-        }
-
-        float speed = 5.f;
-
-        if (!isInBattleMode) {
-            if (input.get_isUp()) y -= speed;
-            if (input.get_isDown()) y += speed;
-            if (input.get_isLeft()) x -= speed;
-            if (input.get_isRight()) x += speed;
         }
 
         window.display();
